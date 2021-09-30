6c96d5204ab8a78faf34f8b72b0e0a8caf2db7d6
iBE4R <korobskoi888@gmail.com>
2021-09-30 14:20:49 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+  
+  $('.top-slider__inner').slick({
+    
+  });
+
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer1 = mixitup(containerEl2, config);
+})
